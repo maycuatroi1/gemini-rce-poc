853d6201fd96367918aa5057f9954e3926547b98
Nguyen Anh Binh <socrat.nguyenanhbinh@gmail.com>
2026-03-11 04:58:36 +0700

    Initial commit: awesome-utils with Gemini CI review

---
+++ README.md
@@ -0,0 +1,26 @@
+# awesome-utils
+
+A collection of useful utility functions for Node.js projects.
+
+## Installation
+
+```bash
+npm install awesome-utils
+```
+
+## Usage
+
+```javascript
+const { slugify, truncate, debounce } = require('awesome-utils');
+
+console.log(slugify('Hello World!')); // hello-world
+console.log(truncate('Long text here', 8)); // Long tex...
+```
+
+## Contributing
+
+Pull requests are welcome! Please read the contributing guidelines first.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,23 @@
+function slugify(text) {
+  return text
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '');
+}
+
+function truncate(str, length) {
+  if (str.length <= length) return str;
+  return str.slice(0, length) + '...';
+}
+
+function debounce(fn, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn.apply(this, args), delay);
+  };
+}
+
+module.exports = { slugify, truncate, debounce };
